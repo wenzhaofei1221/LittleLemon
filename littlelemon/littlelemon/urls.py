cde84fd580368b8c1f47d13210800d5719e928e1
mac <wenzhaofei1221@gmail.com>
2025-05-19 13:49:11 -0400

    may 19 commit2

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -16,8 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework import routers
+from restaurant import views
+
+router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)  
+
+router.register(r'tables', views.bookingView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/',include('restaurant.urls'))
+    path('api/',include('restaurant.urls')),
+    path('api/booking/', include(router.urls)),
+    
+    # path('', include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from .views import MenuItemView, SingleMenuItemView,bookingView
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/',MenuItemView.as_view()),
+    path('menu/<int:pk>',SingleMenuItemView.as_view()),
+    # path('booking/',bookingView.as_view()),
 ]
